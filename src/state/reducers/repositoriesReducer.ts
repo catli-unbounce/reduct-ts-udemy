9d3ba8c351f3c3d7b5ec8f9412317a8c0596622f
catli-unbounce <catherine.li@unbounce.com>
2022-05-19 08:57:10 -0700

    Finished reducer

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,32 +1,59 @@
 interface RepositoriesState {
-    loading: boolean;
-    error: string | null;
-    data: string[];
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+//Create 3 different interfaces for 3 different actions (success, loading, error)
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
-    switch (action.type) {
-        case 'search_repositories':
-            return {
-                loading: true,
-                error: null,
-                data: []
-            }
-        case 'search_repos_success':
-            return {
-                loading: false,
-                error: null,
-                data: action.payload
-            }
-        case 'search_repos_error':
-            return {
-                loading: false,
-                error: action.payload,
-                data: []
-            }
-        default: 
-        return state;
-    }
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPO_SUCCESS;
+  payload: string[];
 }
+
+interface SearchRepositoriesErrorAction {
+  type: ActionType.SEARCH_REPO_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = "search_repositories",
+  SEARCH_REPO_SUCCESS = "search_repos_success",
+  SEARCH_REPO_ERROR = "search_repos_error",
+}
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case ActionType.SEARCH_REPO_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case ActionType.SEARCH_REPO_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
